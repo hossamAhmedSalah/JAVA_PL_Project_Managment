427f856ebd3f0e60b3d6e7ab33fdcf6c4b1dc31b
hossamAhmedSalah <100955137+hossamAhmedSalah@users.noreply.github.com>
2022-12-12 18:41:19 +0200

    nice hossam

--- src/main/java/com/java_pl_project_managment/util/Employee.java
+++ src/main/java/com/java_pl_project_managment/util/Employee.java
@@ -4,14 +4,9 @@ import com.java_pl_project_managment.util.DB.SQLConnection;
 import java.sql.*;
 
 public class Employee extends Account{
-    public String TL_name;
-    
-    SQLConnection conn = new SQLConnection();
 
-    Employee(Account a) throws SQLException{
-        conn.rs = conn.statement.executeQuery("select TL_email from employee inner join tl on employee.tl_id = tl.tl_id where employee.em_email = " + a.email);
-        while(conn.rs.next()){
-            TL_name = conn.rs.getString("tl_email");
-        }
+
+    public Employee() throws SQLException {
+        System.out.println(salary);
     }
 }
