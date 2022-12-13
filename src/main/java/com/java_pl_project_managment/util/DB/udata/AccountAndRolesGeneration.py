11e2b70306a9bbd498c6698b31b0fc3a7c4ee2c3
hossamAhmedSalah <100955137+hossamAhmedSalah@users.noreply.github.com>
2022-12-13 13:46:57 +0200

    Update AccountAndRolesGeneration.py

--- src/main/java/com/java_pl_project_managment/util/DB/udata/AccountAndRolesGeneration.py
+++ src/main/java/com/java_pl_project_managment/util/DB/udata/AccountAndRolesGeneration.py
@@ -2,7 +2,7 @@ from randomuser import RandomUser
 
 
 
-# Generate a list of 10 random users
+# Generate a list of 20 random users
 Salary = 1250.0
 user_list = RandomUser.generate_users(20)
 users_data = [(
