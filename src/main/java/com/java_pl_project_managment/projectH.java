7e82b6af9261aa016a0de7a3e71006cc7299e977
gimmeursocks <bobteen1@gmail.com>
2022-12-16 01:05:30 +0200

    يارب x2

--- src/main/java/com/java_pl_project_managment/projectH.java
+++ src/main/java/com/java_pl_project_managment/projectH.java
@@ -1,79 +1,106 @@
 package com.java_pl_project_managment;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 public class projectH {
     // I would show the Date as a String
-    private String date;
-    private String description;
-    private String pmEmail;
-    private String tlEmail;
-    private String proName;
-    private String state;
-    private Double progress;
+    private SimpleStringProperty date;
+    private SimpleStringProperty description;
+    private SimpleStringProperty pmEmail;
+    private SimpleStringProperty tlEmail;
+    private SimpleStringProperty proName;
+    private SimpleStringProperty state;
+    private SimpleDoubleProperty progress;
 
     public projectH(String date, String description, String pmEmail, String tlEmail, String proName, String state, double progress) {
-        this.date = date;
-        this.description = description;
-        this.pmEmail = pmEmail;
-        this.tlEmail = tlEmail;
-        this.proName = proName;
-        this.state = state;
-        this.progress = progress;
+        this.date = new SimpleStringProperty(date);
+        this.description = new SimpleStringProperty(description);
+        this.pmEmail = new SimpleStringProperty(pmEmail);
+        this.tlEmail = new SimpleStringProperty(tlEmail);
+        this.proName = new SimpleStringProperty(proName);
+        this.state = new SimpleStringProperty(state);
+        this.progress = new SimpleDoubleProperty(progress);
     }
 
     public String getDate() {
-        return date;
+        return date.get();
     }
 
     public void setDate(String date) {
-        this.date = date;
+        this.date.set(date);
     }
 
     public String getDescription() {
-        return description;
+        return description.get();
     }
 
     public void setDescription(String description) {
-        this.description = description;
+        this.description.set(description);
     }
 
     public String getPmEmail() {
-        return pmEmail;
+        return pmEmail.get();
     }
 
     public void setPmEmail(String pmEmail) {
-        this.pmEmail = pmEmail;
+        this.pmEmail.set(pmEmail);
     }
 
     public String getTlEmail() {
-        return tlEmail;
+        return tlEmail.get();
     }
 
     public void setTlEmail(String tlEmail) {
-        this.tlEmail = tlEmail;
+        this.tlEmail.set(tlEmail);
     }
 
     public String getProName() {
-        return proName;
+        return proName.get();
     }
 
     public void setProName(String proName) {
-        this.proName = proName;
+        this.proName.set(proName);
     }
 
     public String getState() {
-        return state;
+        return state.get();
     }
 
     public void setState(String state) {
-        this.state = state;
+        this.state.set(state);
     }
 
     public double getProgress() {
-        return progress;
+        return progress.get();
     }
 
     public void setProgress(Double progress) {
-        this.progress = progress;
+        this.progress.set(progress);
     }
+
+    public StringProperty dateProperty() {
+        return date;
+    }
+    public StringProperty descriptionProperty() {
+        return description;
+    }
+    public StringProperty pmEmailProperty() {
+        return pmEmail;
+    }
+    public StringProperty tlEmailProperty() {
+        return tlEmail;
+    }
+    public StringProperty proNameProperty() {
+        return proName;
+    }
+    public StringProperty stateProperty() {
+        return state;
+    }
+    public DoubleProperty progressProperty() {
+        return progress;
+    }
+    
 }
